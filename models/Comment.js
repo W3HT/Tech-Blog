aad569c7c05758be45ff7024a4b6ab888c9406b7
Frost <mpyang2021@gmail.com>
2021-11-10 21:35:53 -0600

    populated models.comments.js

---
+++ models/Comment.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+Comment.init(
+    {
+        content: {
+            type: DataTypes.TEXT,
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        },
+        
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+
+        updated: {
+            type: DataTypes.BOOLEAN,
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        moduleName: 'comment'
+    }
+
+)
+
+module.exports = Comment;
