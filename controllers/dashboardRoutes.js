2456e1506a034be0a4298e36da489d2458d2facb
Frost <mpyang2021@gmail.com>
2021-11-10 19:18:29 -0600

    added homeRoutes and dashboardRoutes files to controllers

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+// '/'
+
+// '/new'
+
+// '/edit/:id'
+
+//
+
+module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+
+// get all '/'
+
+// get single '/post/:id/
+
+// login '/login'
+
+// signup '/signup'
+
+module.exports = router;
