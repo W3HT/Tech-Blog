69a4f7b15799c6364e5fcb5d199d98527917e22a
Frost <mpyang2021@gmail.com>
2021-11-11 20:38:43 -0600

    updated controllers.api.postRoutes.js

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,14 +3,50 @@ const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
-  // code
+  const body = req.body;
+
+  try {
+    const newPost = await Post.create({ ...body, userId: req.session.userId });
+    res.json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', withAuth, async (req, res) => {
-  // code
+  try {
+    const updatePost = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (updatePost > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', withAuth, async (req, res) => {
-  // code
+  try {
+    const deletePost = Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (deletePost > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 module.exports = router;
