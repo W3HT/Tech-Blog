3c03379df8f95885fb3bfec07ff26e3b7c3c4e1b
Frost <mpyang2021@gmail.com>
2021-11-11 22:12:54 -0600

    updated controllers.dashboardRoutes.js

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,10 +1,68 @@
 const router = require('express').Router();
+const { Post } = require('../models/');
+const withAuth = require('../utils/auth');
+
+
 
 // '/'
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {
+                userId: req.session.user_id,
+
+            },
+        })
+    
+        const posts = postData.map((post) => post.get({ plain: true}));
+
+        res.render('dashboard', {posts, logged_in: req.session.logged_in});
+        
+    } catch (err) {
+        res.redirect('login');
+    }
+});
+
 
 // '/new'
+router.get('/new', withAuth, (req, res) => {
+    res.render('newPost', {logged_in: req.session.logged_in})
+})
 
 // '/edit/:id'
+// router.get('/viewPost/:id', withAuth, (req, res) => {
+    // try {
+    //     const postData = await Post.findByPk(req.params.id);
+
+    //     if (postData) {
+    //         const post = postData.get({ plain: true});
+
+    //     res.render('viewPost', {logged_in: req.session.logged_in});
+    //     }
+    // } catch (err) {
+    //     res.redirect('login');
+    // }
+router.get('/viewPost/:id', async (req,res) =>{
+    const postData = Post.findByPk(req.params.id, {
+        include:[
+            User,
+            {
+                model: Comment,
+                include: [User]
+            }
+        ]
+    })
+    const comments = postData.comments.map(postComment => {
+        const cleanUser = postComment.user.get({plain: true});
+        let comment = postComment.get({plain: true});
+
+        comment.user = cleanUser
+        return comment
+    });
+    let post = postData.get({plain: req.session.logged_in});
+    post.comments = comments 
+    res.render('vewPost', {logged_in: req.session.logged_in});)
+ });
 
 //
 
