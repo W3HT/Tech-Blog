e0134fb280dd91769cfff0c08df1ae442be3f3ed
Frost <mpyang2021@gmail.com>
2021-11-16 19:26:37 -0600

    debugging renders

---
+++ utils/helpers.js
@@ -0,0 +1,10 @@
+module.exports ={
+    // format_time: (Date) => {
+    //     return Date.toLocaleTimeString();
+    // },
+    // format_date: (date) => {
+    //     return `${new Date(date).getMonth() =1}/${new Date(date).getDate()}/${
+    //         new Date(date).getFullYear()
+    //     }`;
+    // },
+};
